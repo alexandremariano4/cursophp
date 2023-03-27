275c761cf5fb6af449f5ffba00603aea9314527e
Alexandre Mariano <alexandre_mariano@hotmail.com.br>
2023-03-27 16:57:34 -0300

    operaçoes matemáticas

---
+++ geral/funcoes_matematicas.php
@@ -0,0 +1,27 @@
+<?php 
+
+    $resultado = abs(-500);
+    echo "Absoluto -> $resultado \n";
+    
+    $resultado = base_convert(500, 10, 2);
+    echo "Absoluto -> $resultado \n";
+    
+    $resultado = intdiv(5,2); //Divisão inteira
+    echo "Divisão inteira -> $resultado \n";
+    
+    $resultado = min(5,20,5,6,4); 
+    echo "Valor mínimo -> $resultado \n";
+    
+    $resultado = max(5,20,7,9,46,73); 
+    echo "Absoluto -> $resultado \n";
+    
+    $resultado = pi(); // M_PI
+    echo "Absoluto -> $resultado \n";
+    
+    $resultado = pow(3,3); //$resultado = 2 ** 3 ;
+    echo "Absoluto -> $resultado \n";
+    
+    $resultado = sqrt(81);
+    echo "Absoluto -> $resultado \n";
+
+?>
