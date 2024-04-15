f497199c7c7de791e6ea21ff5f2176aabe1a71c8
Alexandre <alexandre_mariano@hotmail.com.br>
2024-04-15 10:17:20 -0300

    .

--- especializati/uploads/multiplos_arquivos.php
+++ especializati/uploads/multiplos_arquivos.php
@@ -10,10 +10,9 @@ if (!is_dir($path)){
 for ($i = 0; $i < count($photos['name']); $i++){
     if (move_uploaded_file($photos['tmp_name'][$i], "$path/".$photos['name'][$i] )){
         echo 'Sucesso!';
-        echo '<br>';
     }
     else{
         echo 'Falha!';
-        echo '<br>';
     }
+    echo '<br>';
 }
